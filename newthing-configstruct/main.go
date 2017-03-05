4f05586ceee4d947cc1297b395e751d0d34ceb59
Coleman McFarland <coleman.mcfarland@gmail.com>
2017-03-05 14:41:26 -0500

    Configuration of a Thing from a json file

---
+++ newthing-configstruct/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+
+	conf, err := NewConfig("conf.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t := NewThing(conf)
+
+	log.Printf("thing initialized: %v", t)
+}
+
+type Config struct {
+	Foo string
+	Baz int
+}
+
+// NewConfig reads a json config file.
+func NewConfig(path string) (Config, error) {
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return Config{}, err
+	}
+
+	// Declare a config. Initial values are the "zero values".
+	var conf Config
+	// Pass a pointer to conf to Unmarshal. Read data into it.
+	err = json.Unmarshal(data, &conf)
+	if err != nil {
+		return Config{}, err
+	}
+	// conf has data
+	return conf, nil
+}
+
+// Thing is our thing that needs configuration.
+type Thing struct {
+	Msg string
+}
+
+// NewThing is our Thing constructor.
+func NewThing(conf Config) *Thing {
+
+	// Declare our Thing
+	var t Thing
+	// Set our field
+	t.Msg = fmt.Sprintf("Foo is %s and Baz is %v", conf.Foo, conf.Baz)
+	// Return a pointer to our Thing
+	return &t
+}
